a3ee0403b3639bcdfa736b9bc26e97c6974416ce
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-09 05:45:02 +0100

    Create Exercise 3_inheritance.js

---
+++ OOP/Exercise 3_inheritance.js
@@ -0,0 +1,66 @@
+ 
+// Inheritance is the process of passing methods and properties from one class to another.
+// In JavaScript we passed the prototype property of one constructor to another constructor.
+// use Object.create instead new keyword.
+
+
+// 1 - Create a constructor function for a Vehicle. Each vehicle should have a make, model and year property.
+
+function Vehicle(make,model,year){
+	this.make = make;
+	this.model = model;
+	this.year = year;
+}
+
+// 2 - Add a function to the Vehicle prototype called start which returns the string "VROOM!"
+
+Vehicle.prototype.start = function(){
+	return "VROOM!";
+}
+
+// 3 - Add a function to the Vehicle prototype called toString which returns the string "The make, model, and year are" 
+// concatenated with the make, model and year property
+
+Vehicle.prototype.toString = function(){
+	return "The make, model, and year are "+this.make+ " " +this.model+" "+this.year;
+}
+
+
+// 4 - Create a constructor function for a Car. Each object created from the Car function should also have a make, model,
+//  and year and a property called numWheels which should be 4. The Car prototype should inherit all of the methods from
+//  the Vehicle prototype
+
+function Car (){
+	Vehicle.apply(this,arguments);
+	this.numWheels = 4;
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car; // make sure that the constructor is reset for the current prototype
+
+// 5 - Create a constructor function for a Motorcycle. Each object created from the Motorcycle function should also have 
+// a make, model, and year and a property called numWheels which should be 2. The Motorcycle prototype should inherit all
+// of the methods from the Vehicle prototype
+
+function Motorcycle (){
+	Vehicle.apply(this,arguments);
+	this.numWheels = 2;
+}
+
+Motorcycle.prototype = Object.create(Vehicle.prototype);
+Motorcycle.prototype.constructor = Motorcycle; 
+
+
+
+
+
+//Recap OOP:
+//Every time the 'new' keyword is used, a link between the object created and the prototype property of the constructor
+// is established - this link can be accessed using __proto__
+
+//The prototype object contains a property called constructor, which points back to the constructor function.
+
+// To share properties and methods for objects created by a constructor function, place them in the prototype as it is the most efficient.
+
+// To pass mergeAttributes and properties from one prototype object to another, we can use inheritance which involves setting
+// the prototype property to be a newly created object using Object.create and reseting the constructor property. 
